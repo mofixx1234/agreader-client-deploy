f1c84cbe09f4597346da97c5e1a3cecbe9bf3ed7
mofixx1234 <105564840+mofixx1234@users.noreply.github.com>
2026-05-18 12:33:12 +0000

    Update http.ts

--- src/api/http.ts
+++ src/api/http.ts
@@ -29,7 +29,7 @@ import type{
 // ENV
 // =========================================================
 
-const rawApiBaseUrl = "http://81.0.220.161:4001/api/v1"
+const rawApiBaseUrl = "http://81.0.220.161:4000/api/v1"
 // const rawApiBaseUrl = import.meta.env.VITE_API_BASE_URL?.trim()
 const API_BASE_URL = (rawApiBaseUrl || '/api/v1').replace(/\/+$/, '')
 
